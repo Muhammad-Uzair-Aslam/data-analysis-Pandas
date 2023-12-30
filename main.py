0f2650dbf171ef873c013bad670008cb8874ad7a
Uzair Aslam <uzairaslam990@gmail.com>
2023-12-30 01:52:15 -0800

    classifiers use

--- main.py
+++ main.py
@@ -7,24 +7,16 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import tree
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
-
 X, y = load_wine(return_X_y=True)
-
-W,x=load_diabetes(return_X_y=True)
-
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.4,random_state=0,shuffle=False)
 print("Training Data:",X_train.shape)
 print(X_test.shape)
 from sklearn import svm
-#A_train,A_test,c_train,c_test=train_test_split(A,c,test_size=0.4,random_state=0,shuffle=False)
-#print("Training Data:",X_train.shape)
-#print(X_test.shape)
-W_train,W_test, x_train , x_test=train_test_split(W,x,test_size=0.4,random_state=0,shuffle=False)
+
 print("Training Data:",X_train.shape)
 print(X_test.shape)
-
-
 clf = svm.SVC()
+
 dt=tree.DecisionTreeClassifier() #dt initialize
 rf=RandomForestClassifier()   #rf initialize
 lr=LogisticRegression()     #lr initialize
